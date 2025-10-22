a8e6aae6fc6f6aa629e5cf18bba00b5b182b696d
Valeriia Fedorenko <xaarlox@gmail.com>
2025-10-22 17:36:14 +0300

    feat(models): add Submission & Attempt; update Lab model

--- labs/admin.py
+++ labs/admin.py
@@ -1,5 +1,7 @@
 from django.contrib import admin
-from .models import Lab
+from .models import Lab, Submission, Attempt
 
 
 admin.site.register(Lab)
+admin.site.register(Submission)
+admin.site.register(Attempt)
--- labs/models.py
+++ labs/models.py
@@ -5,11 +5,51 @@ import uuid
 class Lab(models.Model):
     title = models.CharField(max_length=200)
     description = models.TextField(null=True, blank=True)
+    order = models.IntegerField(default=0)
+    passing_mark = models.FloatField(null=True, blank=True)
     max_mark = models.FloatField(null=True, blank=True)
+    max_attempts = models.IntegerField(default=3)
     created_at = models.DateTimeField(auto_now_add=True)
-    updated_at = models.DateTimeField(auto_now=True)
+    deadline = models.DateTimeField(null=True, blank=True)
     is_active = models.BooleanField(default=False)
     id = models.UUIDField(default=uuid.uuid4, unique=True, primary_key=True, editable=False)
 
+
+    class Meta:
+        ordering = ['order']
+
+
     def __str__(self):
         return self.title
+    
+
+class Submission(models.Model):
+    lab = models.ForeignKey(Lab, on_delete=models.CASCADE, related_name='submissions')
+    # profile = 
+    status = models.CharField(max_length=100, default='not submitted')
+    best_mark = models.FloatField(default=0, null=True, blank=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    attempts_left = models.IntegerField()
+    id = models.UUIDField(default=uuid.uuid4, unique=True, primary_key=True, editable=False)
+
+
+    def __str__(self):
+        return f"Submission for {self.lab.title} by"
+
+
+class Attempt(models.Model):
+    submission = models.ForeignKey(Submission, on_delete=models.CASCADE, related_name='attempts')
+    mark = models.FloatField(default=0, null=True, blank=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    attempt_number = models.IntegerField()
+    answers = models.JSONField(null=True, blank=True)
+    id = models.UUIDField(default=uuid.uuid4, unique=True, primary_key=True, editable=False)
+
+
+    class Meta:
+        ordering = ['created_at']
+
+    
+    def __str__(self):
+        return f"Attempt #{self.attempt_number} for submission ID {self.submission.id}"
