1dadb34912bc23ada95656bbfd585a9af6504878
Valeriia Fedorenko <xaarlox@gmail.com>
2025-10-22 14:12:55 +0300

    feat: add Lab model & configure Django admin

--- labs/models.py
+++ labs/models.py
@@ -1,3 +1,15 @@
 from django.db import models
+import uuid
 
-# Create your models here.
+
+class Lab(models.Model):
+    title = models.CharField(max_length=200)
+    description = models.TextField(null=True, blank=True)
+    max_mark = models.FloatField(null=True, blank=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    is_active = models.BooleanField(default=False)
+    id = models.UUIDField(default=uuid.uuid4, unique=True, primary_key=True, editable=False)
+
+    def __str__(self):
+        return self.title
