324918114fbbc896f19b1f33efef8415a9e6111b
Valeriia Fedorenko <xaarlox@gmail.com>
2025-10-24 17:24:14 +0300

    feat(ui): add base home & labs pages with images & styles

--- labs/urls.py
+++ labs/urls.py
@@ -2,9 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.labs, name="labs"),
+    path('', views.home, name="home"),
+    path('labs/', views.labs, name="labs"),
     path('lab/<str:pk>/', views.lab, name="lab"),
-    path('create-lab/', views.createLab, name="create-lab"),
-    path('update-lab/<str:pk>/', views.updateLab, name="update-lab"),
-    path('delete-lab/<str:pk>/', views.deleteLab, name="delete-lab"),
 ]
--- labs/views.py
+++ labs/views.py
@@ -1,7 +1,10 @@
 from django.shortcuts import render, redirect
 from django.http import HttpResponse
 from .models import Lab
-from .forms import LabForm
+
+
+def home(request):
+    return render(request, 'home.html')
 
 
 def labs(request):
@@ -12,36 +15,4 @@ def labs(request):
 
 def lab(request, pk):
     labObj = Lab.objects.get(id=pk)
-    return render(request, 'labs/single-lab.html', {'lab': labObj})
-
-
-def createLab(request):
-    form = LabForm()
-    if request.method == 'POST':
-        form = LabForm(request.POST)
-        if form.is_valid():
-            form.save()
-            return redirect('labs')
-    context = {'form': form}
-    return render(request, "labs/lab_form.html", context)
-
-
-def updateLab(request, pk):
-    lab = Lab.objects.get(id=pk)
-    form = LabForm(instance=lab)
-    if request.method == 'POST':
-        form = LabForm(request.POST, instance=lab)
-        if form.is_valid():
-            form.save()
-            return redirect('labs')
-    context = {'form': form}
-    return render(request, "labs/lab_form.html", context)
-
-
-def deleteLab(request, pk):
-    lab = Lab.objects.get(id=pk)
-    if request.method == 'POST':
-        lab.delete()
-        return redirect('labs')
-    context = {'object': lab}
-    return render(request, 'labs/delete_template.html', context)
+    return render(request, 'labs/lab.html', {'lab': labObj})
